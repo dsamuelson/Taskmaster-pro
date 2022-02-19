b98a9c803f348ac1cd1282d9f2d6dfe5bdd95e53
David Samuelson <linksn.1fan@gmail.com>
2022-02-19 07:32:03 -0700

    fixed timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -251,13 +251,15 @@ var auditTask = function(taskEl) {
   } else if (Math.abs(moment().diff(time, "days")) <= 2){
     $(taskEl).addClass("list-group-item-warning");
   }
+
+  setInterval(function() {
+    $(".card .list-group-item").each(function(index, el) {
+      auditTask(el);
+    });
+  },((1000 * 60) * 30));
 };
 
-setInterval(function() {
-  $(".card .list-group-item").each(function(index, el) {
-    auditTask(el);
-  });
-}, ((1000 * 60) *30));
+
 
 // load tasks for the first time
 loadTasks();
